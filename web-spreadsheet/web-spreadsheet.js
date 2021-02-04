128b2b886eba26834a245d71a31cc1a3ad09110d
Kuba Smorąg <qbaiprzyjaciele@gmail.com>
2021-02-04 01:56:07 +0100

    lit-element web-spreadsheet POC

---
+++ web-spreadsheet/web-spreadsheet.js
@@ -0,0 +1,370 @@
+<html>
+<head>
+  <!-- Polyfills only needed for Firefox and Edge. -->
+  <script src="https://unpkg.com/@webcomponents/webcomponentsjs@latest/webcomponents-loader.js"></script>
+</head>
+<body>
+  <!-- Works only on browsers that support Javascript modules like
+       Chrome, Safari, Firefox 60, Edge 17 -->
+  
+<!--   <script>
+    const expressionParser () => {
+      var expressionependencies = [];
+      var ast;
+      
+      const compileExpression (expression) => {  
+        ast = buildAst(expression); 
+      };
+      
+      const addDependencies (ast) => {
+        
+      };
+      
+      const buildAst = (expression) => {
+        retrun {};
+      };
+      
+      const evaluate(params, ast) => {
+        
+      };
+      
+      return {
+        parse: (expression) => {
+          
+        },
+        evaluate: (params) => {
+          return evaluate(params, ast)
+        }
+      }
+    }
+  </script> -->
+  <script type="module">
+    import {LitElement, html, css} from 'https://unpkg.com/lit-element/lit-element.js?module';
+    
+    class MyElement extends LitElement {
+
+      static get properties() {
+        return {
+          mood: {type: String }
+        }
+      }
+      
+      static get styles() {
+        return css`.mood { color: green; }`;
+      }
+
+      render() {
+        return html`Web Components are <span class="mood">${this.mood}</span>!`;
+      }
+      
+    }
+
+    customElements.define('my-element', MyElement);
+  </script>
+  
+  <script type="module">
+    import {LitElement, html, css} from 'https://unpkg.com/lit-element/lit-element.js?module';
+    
+    class SpreadSheetInput extends LitElement {
+
+      static get properties() {
+        return {
+          mood: {type: String },
+          formula: { 
+            type: Function 
+          },
+          value: {
+            type: Number,
+          },
+          onChange: {
+            type: Function,
+          },
+        }
+      }     
+      
+      render() {
+        return html`<input type="number" value=${this.value} @change=${this.onChange} ></input>`;
+      }
+      
+    }
+
+    customElements.define('spread-sheet-input', SpreadSheetInput);
+  </script>
+  
+  <script type="module">
+    import {LitElement, html, css} from 'https://unpkg.com/lit-element/lit-element.js?module';
+    
+    class SpreadSheetGrid extends LitElement {
+      constructor() {
+        super();
+        
+        this.subscribersMap = {};
+      
+        const addSubscription = (subscribeToX, subscribeToY, observerX, observerY) =>  {
+          //this.subscribersMap[subscribeToX][subscribeToY] = ;
+          if(!this.subscriberMap[subscribeToX]) {
+            this.subscriberMap[subscribeToX] = {};
+          }
+          if(!this.subscriberMap[subscribeToX][subscribeToY]) {
+            this.subscriberMap[subscribeToX][subscribeToY] = [];
+          }
+          this.subscriberMap.length 
+          for(let i=0; i<this.subscriberMap[subscribeToX][subscribeToY].length; i++) {
+            const [ existingSubscriberX, existingSubscriberY] = this.subscriberMap[subscribeToX][subscribeToY][i];
+            if(existingSubscriberX == observerX && observerY == existingSubscriberY) {
+              // observer already added
+              return;
+            }
+          }
+          this.subscriberMap[subscribeToX][subscribeToY].push([observerX, observerY]);
+        }
+        
+        const addFormula = (x, y, formula) => {
+          this.cells[x][y].formula = formula;
+          formula.formulaDependencies.forEach(dep => {
+            addSubscription([...dep]);
+          });
+          this.cells.value = this.cells.formula.formulaExpression(this.cells);
+        }
+        
+        const updateValue = (x,y, value) => {
+          
+        };
+        
+        this.cells = [
+          [
+            { value: 1.05, formula: null },
+            { value: 2.02, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { 
+             value: null, 
+             formula: {
+               formulaExpression: (cells) => {
+                cells[0][1] + cells[0][2];
+              },
+              formulaDependencies: [ [0,1], [0,2] ],
+             },
+            },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+          [
+            { value: 234.32, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+            { value: 324.83, formula: null },
+          ],
+        ]
+      }
+     
+      static get properties() {
+        return {
+          formula: { 
+            type: Function 
+          },
+          value: {
+            type: Number,
+          },
+        }
+      }     
+      
+      render() {
+        return html`<table>
+          ${this.cells.map((row, index) => html`<tr>${this.renderRow(row, index)}</tr>`)}
+        </table>`;
+      }
+      
+//       reavaluate(cells) {
+//         for(let i=0; i<cells.length; i++) {
+//         }
+//       }
+      
+//       evaluateFormula(cells, cellToEvaluate) {
+//       }
+      
+      
+      notifyValueChange(row,column, value) {
+        //syncup
+        this.cells[row][column] = value;
+        if(this.subscribersMap[row]) {
+          if(this.subscribersMap[row][column]) {
+            this.subscribersMap[row][column].forEach( observer => {
+              const [observerRow, observerColumn] = observer;
+              this.updateObserverValue(observerRow, observerColumn);
+              
+            });
+          }
+        }
+      }
+      
+      syncDown(row, column, value) {
+        const elementToUpdate = this.shadowRoot.getElementById(`cell_${row}_${column}`);
+        elementToUpdate.value = value;
+      }
+      
+      updateObserverValue(observerRow, observerColumn) {
+        const cell = this.cells[observerRow][observerColumn];
+        const { formula } = cell;
+        const value = formula.formulaExpression(this.cells);
+        cell.value = value;
+        this.syncDown(observerRow, observerColumn, value);
+      }
+      
+     
+      
+      renderRow(row, rowIndex) {
+        return html`
+         ${row.map( (cell, index) => html`
+         <td>
+          <spread-sheet-input id=${`cell_${rowIndex}_${index}`} .value=${cell.value} .onChange=${(e) => { 
+            console.log(`changed = ${e.target.value}`);
+            this.notifyValueChange(rowIndex, index); 
+          }}>
+          </spread-sheet-input>
+         </td>`)}
+        `;
+      }
+    }
+
+    customElements.define('spread-sheet-grid', SpreadSheetGrid);
+  </script>
+  
+  <my-element mood="great"></my-element>
+  <spread-sheet-input value="41234"></spread-sheet-input>
+  <br/>
+  <spread-sheet-grid></spread-sheet-grid>
+  
+</body>
+</html>
